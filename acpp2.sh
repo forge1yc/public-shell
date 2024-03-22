fedd8f69acd8a75ec9923379aa5010dabd3d73cf
houyichao <houyichaochao@gmail.com>
2024-03-22 10:05:48 +0800

    feat(Code): 新增acpp2

---
+++ acpp2.sh
@@ -0,0 +1,42 @@
+##########################################################################
+# File Name: acpp2.sh
+# Author: Fuge And ZiYan
+# mail: houyichaochao@gmail.com
+# Created Time: 2024-03-22 10:03:58 
+# Last modified: 2024-03-22 10:04:05 
+# Description: 增加XML文件索引 FOR ZiYan
+#########################################################################
+#!/bin/sh
+trap 'echo "Received SIGINT signal and exiting..."; exit 1' INT
+
+TYPE=$(gum choose "run" "feat" "amend" "fix")
+
+current_dir=$(pwd)
+PROJECT=$(echo "$current_dir" | rev | cut -d'/' -f1 | rev)
+
+
+# Commit these changes
+if [ $TYPE = "run" ]
+then
+    # Debian/Ubuntu 系统：sudo apt-get install xmlstarlet
+    # Red Hat/CentOS 系统：sudo yum install xmlstarlet
+    # macOS（通过 Homebrew）：brew install xmlstarlet
+    PORT=$(xmlstarlet sel -t -m "//mapMapping[sourceLocation/path[contains(.,'$PROJECT')]]" -v "destLocation/port" -n remote.xml)
+
+    if [[ -z $PORT ]]; then
+        echo "PROJECT $PROJECT MATCH PORT NOT FOUND，CHECK PORJECT, EXIT!"
+        exit
+    else
+        echo "PORT: $PORT"
+    fi
+    echo $PROJECT && npm run start -- --port $PORT
+
+elif [ $TYPE = "amend" ]
+then
+gum confirm "$TYPE?" && git add . && git commit --amend && git push -f && def p -d
+
+else
+SUMMARY=$(gum input --value "$TYPE: " --placeholder "Summary of this change")
+gum confirm "$SUMMARY?" && git add . &&  git commit -m "$SUMMARY" && git push && def p -d
+
+fi
--- test.sh
+++ test.sh
@@ -7,3 +7,24 @@
 #########################################################################
 #!/bin/bash
 echo "hello world"
+
+current_folder="cloud-ram-widget-permission"
+# sed -nE '/<sourceLocation>.*'$current_folder'.*<\/sourceLocation>/{n;n;n;s/[^0-9]//gp}' remote.xml
+
+# sed -nE '/<sourceLocation>.*'$current_folder'.*<\/sourceLocation>/{n;n;n;s/[^0-9]//gp}' remote.xml
+
+# port=$(awk -F'[<>]' -v folder="$current_folder" '$2 == "sourceLocation" && $6 ~ folder {getline; getline; if ($2 == "port") print $3}' remote.xml)
+
+# port=$(awk -F'[<>]' -v folder="$current_folder" '$2 == "sourceLocation" && $6 ~ folder {found=0} $2 == "port" && found==1 {print $3} $2 == "path" && $6 ~ folder {found=1}' remote.xml)
+# 转换通配符为正则表达式
+# regex_folder=$(echo "$current_folder" | sed 's/\*/.*/g')
+
+# 使用awk匹配符合条件的行，并提取对应的端口号
+# port=$(awk -F'[<>]' -v folder="$regex_folder" '$2 == "sourceLocation" && $6 ~ folder {found=0} $2 == "port" && found==1 {print $3} $2 == "path" && $6 ~ folder {found=1}' remote.xml)
+# port=$(awk -F'[<>]' -v folder="$current_folder" '$2 == "sourceLocation" && $6 ~ folder {getline; getline; if ($2 == "port") print $3}' remote.xml)
+
+
+port=$(xmlstarlet sel -t -m "//mapMapping[sourceLocation/path[contains(.,'$current_folder')]]" -v "destLocation/port" -n remote.xml)
+
+
+echo "Port found: $port"
